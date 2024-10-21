7c515be8728926289925f31b177b3af84c627dad
TLMphahlele <222124059@edu.vut.ac.za>
2024-10-21 18:48:47 +0200

    Update vut_ai_student_assistance.py

--- AI Solution Code/vut_ai_student_assistance.py
+++ AI Solution Code/vut_ai_student_assistance.py
@@ -1,1 +1,21 @@
-
+import numpy as np
+import pandas as pd
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import pyaudio
+from nltk.stem import WordNetLemmatizer
+import pygame
+from datetime import datetime, timedelta
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, classification_report
+from statsmodels.tsa.arima.model import ARIMA
+from datetime import datetime, timedelta
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
